282cb70f81eeec3b92efc98f8b3d97d5046d41a3
Steven Weller <stevenweller091@gmail.com>
2026-04-02 03:59:11 -0400

    removed hash scroll

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import Hero from '../components/Hero';
 import InfoSection from '../components/InfoSection';
-import Instructor from '../components/Instructor';
+// import Instructor from '../components/Instructor';
 
 const Home = () => {
-    const { hash } = useLocation();
-
-    useEffect(() => {
-        if (hash === '#instructors') {
-            const el = document.getElementById('instructors');
-            el?.scrollIntoView({ behavior: 'smooth' });
-        }
-    }, [hash]);
 
     return (
         <>
