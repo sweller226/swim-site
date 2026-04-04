ec09258e9a1db4acd5c9282e468d9eeaed60ba44
Steven Weller <stevenweller091@gmail.com>
2026-04-04 02:58:28 -0400

    added prepend

--- frontend/src/lib/emotion-cache.ts
+++ frontend/src/lib/emotion-cache.ts
@@ -1,5 +1,8 @@
 import createCache from '@emotion/cache';
 
 export default function createEmotionCache() {
-  return createCache({ key: 'mui-style' });
+  return createCache({
+    key: 'mui',
+    prepend: true,
+  });
 }
